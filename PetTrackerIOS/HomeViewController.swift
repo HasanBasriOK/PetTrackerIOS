bcc0207cf1567686a660b776ab847a0f8ffc481f
hasanbasri.ok <okhasanbasri@gmail.com>
2023-07-04 21:03:47 +0300

    initial commit

---
+++ PetTrackerIOS/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  PetTrackerIOS
+//
+//  Created by HasanBasriOk on 4.07.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+}
